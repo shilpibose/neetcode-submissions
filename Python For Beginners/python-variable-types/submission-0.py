67d8400991886a720ddbe3b8ba1d89e003bd6fab
shilpibose <sbose@seas.upenn.edu>
2026-05-30 19:21:27 -0400

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+
+integer_type = 25
+float_type =98.6
+bool_type = False
+string_type = "Alice"
+list_type = [2,4,5]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
