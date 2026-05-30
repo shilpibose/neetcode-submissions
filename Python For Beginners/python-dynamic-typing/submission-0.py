374d9b01b99477fb7916b32cc59e2d983466f022
shilpibose <sbose@seas.upenn.edu>
2026-05-30 19:23:13 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,15 @@
+
+variable = 10
+print(type(variable))
+
+variable = 1.5
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = "False"
+print(type(variable))
+
+variable = [30, 5]
+print(type(variable))
